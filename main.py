7022731fdfbed02df9d3d01da3d8c9a99cc59a70
VladislavVenin <d4spda@yandex.ru>
2025-08-26 19:52:50 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 
 
-host = 'https://wttr.in'
-places = ['san-francisco', 'london', 'svo', 'Череповец']
-params = "?nTqmM&lang=ru"
-for i in places:
-    get = f"/{i}{params}"
-    response = requests.get(host+get)
-    print(response.text)
+def main():
+    host = 'https://wttr.in'
+    places = ['san-francisco', 'london', 'svo', 'Череповец']
+    params = "?nTqmM&lang=ru"
+    for i in places:
+        get = f"/{i}{params}"
+        response = requests.get(host+get)
+        print(response.text)
+
+
+if __name__ == '__main__':
+    main()
