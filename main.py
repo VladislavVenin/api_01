649fb50713fbbc177652a7aac4c4edab4e3da790
VladislavVenin <d4spda@yandex.ru>
2025-08-28 23:16:24 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import requests
+import urllib.parse
 
 
 def main():
     host = 'https://wttr.in'
-    places = ['san-francisco', 'london', 'svo', 'Череповец']
-    params = "?nTqmM&lang=ru"
-    for i in places:
-        get = f"/{i}{params}"
-        response = requests.get(host+get)
-        print(response.text)
+    places = ['san francisco', 'london', 'svo', 'Череповец']
+    keys = {
+        "nTqmM": "",
+        "lang": "ru",
+    }
+    for place in places:
+        try:
+            response = requests.get(urllib.parse.urljoin(host, place), params=keys)
+        except:
+            print("bad url")
+            break
+        if response.status_code == 200 and response.ok is True:
+            print(response.text)
 
 
 if __name__ == '__main__':
