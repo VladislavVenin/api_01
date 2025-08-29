f78c8bed05f22ea0b46583636e64392a633215f5
VladislavVenin <d4spda@yandex.ru>
2025-08-29 23:47:36 +1100

    Добавил возможность ввода с консоли

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import urllib.parse
 
 
 def main():
-    host = 'https://wttr.in'
-    places = ['san francisco', 'london', 'svo', 'Череповец']
-    keys = {
-        "nTqmM": "",
-        "lang": "ru",
-    }
+    host = input("Enter url(Leave the field blank to search https://wttr.in.): ") or "https://wttr.in"
+    places = input("Enter places separated by space: ").split()
+    keys = {}
+    while True:
+        key = input("Enter a key (or press Enter to finish): ")
+        if key == "":
+            break
+        keys[key] = input(f"Enter the value for '{key}': ")
+
     for place in places:
         try:
             response = requests.get(urllib.parse.urljoin(host, place), params=keys)
@@ -21,5 +24,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
